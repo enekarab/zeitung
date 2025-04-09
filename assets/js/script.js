44272416bc89d78c5bc005f3bd4c286429cb8a2a
enekarab <116077816+enekarab@users.noreply.github.com>
2025-04-09 14:16:54 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var myCarousel = new bootstrap.Carousel(document.querySelector('#carouselExampleCaptions'), {
+    interval: 3000,
+    wrap: true
+});
+
+function toggleMenu() {
+    const nav = document.getElementById('burgerNav');
+    nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
+}
+
+function showModal(imageUrl) {
+    document.getElementById('modalImage').src = imageUrl;
+    document.getElementById('overlay').style.display = 'flex';
+  }
+
+  function closeModal() {
+    document.getElementById('overlay').style.display = 'none';
+  }
+
+  document.addEventListener('keydown', function(event) {
+    if (event.key === "Escape") {
+      closeModal();
+    }
+  });
